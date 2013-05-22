a2eb0e9878c8b38a74a81229b050f41779d183f2
Angel Mirkovski <angel.mirkovski@stud.sbg.ac.at>
2013-05-22 16:01:02 +0200

    Adding Milestone4

---
+++ Milestone4.java
@@ -0,0 +1,20 @@
+public class Milestone4{
+	
+	//@F
+	public int f;
+	
+	//@F
+	public int g;
+	
+	//@F
+	public int[] arr;
+	
+	/*@M
+	public int setParameters(int f, int g){//, int[] arr){
+		f = f;
+		g = g;
+		//this.arr = arr;
+		return f;
+	}*/
+}
+
