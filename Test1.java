aac94a36dbeef020264bc0ce8aea07275bfa9a9e
Angel Mirkovski <angel.mirkovski@stud.sbg.ac.at>
2013-05-22 18:29:38 +0200

    test

--- Test1.java
+++ Test1.java
@@ -5,7 +5,7 @@ public class Test1{
 	
 	public static void main(String[] args){
 			try {
-			Parser p = new Parser("./Test.java");
+			Parser p = new Parser("./Milestone4.java");
 			}catch (Exception e) {
 			e.printStackTrace();
 		}
